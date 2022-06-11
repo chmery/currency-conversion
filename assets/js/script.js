1f8ae3e5d9e0b311801ba574a97bbf98749215ab
chmery <104914995+chmery@users.noreply.github.com>
2022-06-11 10:29:40 +0200

    Update code formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-const fromCurrencySelect = document.querySelector('#from');
-const toCurrencySelect = document.querySelector('#to');
-const amount = document.querySelector('#amount');
-const currencyExchangeResult = document.querySelector('#currency-exchange-result');
+const fromCurrencySelect = document.querySelector("#from");
+const toCurrencySelect = document.querySelector("#to");
+const amount = document.querySelector("#amount");
+const currencyExchangeResult = document.querySelector("#currency-exchange-result");
 
-const switchBtn = document.querySelector('.exchange__switch-btn');
-const exchangeBtn = document.querySelector('.exchange__btn');
+const switchBtn = document.querySelector(".exchange__switch-btn");
+const exchangeBtn = document.querySelector(".exchange__btn");
 
-const fromCurrencySelectImg = document.querySelector('#select-img-one');
-const toCurrencySelectImg = document.querySelector('#select-img-two');
+const fromCurrencySelectImg = document.querySelector("#select-img-one");
+const toCurrencySelectImg = document.querySelector("#select-img-two");
 
 const arrowUp = "assets/images/up.svg";
 const arrowDown = "assets/images/down.svg";
@@ -31,43 +31,49 @@ const switchSelectIcon = () => {
         toCurrencySelectOpened = false;
     }
 
-    if(fromCurrencySelectOpened == false) {
+    if (fromCurrencySelectOpened == false) {
         fromCurrencySelectImg.src = arrowDown;
-    } 
+    }
 
-    if(toCurrencySelectOpened == false) {
+    if (toCurrencySelectOpened == false) {
         toCurrencySelectImg.src = arrowDown;
     }
 };
 
-window.addEventListener('click', switchSelectIcon);
-fromCurrencySelect.addEventListener('click', () => fromCurrencySelectOpened = !fromCurrencySelectOpened);
-toCurrencySelect.addEventListener('click', () => toCurrencySelectOpened = !toCurrencySelectOpened);
+window.addEventListener("click", switchSelectIcon);
+fromCurrencySelect.addEventListener("click", () => (fromCurrencySelectOpened = !fromCurrencySelectOpened));
+toCurrencySelect.addEventListener("click", () => (toCurrencySelectOpened = !toCurrencySelectOpened));
 
 const switchCurrencies = () => {
     [fromCurrencySelect.value, toCurrencySelect.value] = [toCurrencySelect.value, fromCurrencySelect.value];
-    if (amount.value != 0){
-    exchangeCurrencies();
-}};
+    if (amount.value != 0) {
+        exchangeCurrencies();
+    }
+};
 
 const exchangeCurrencies = () => {
-    const fromCurrencySelectValue = (fromCurrencySelect.value).toLowerCase();
-    const toCurrencySelectValue = (toCurrencySelect.value).toLowerCase();
+    const fromCurrencySelectValue = fromCurrencySelect.value.toLowerCase();
+    const toCurrencySelectValue = toCurrencySelect.value.toLowerCase();
 
-    const exchangeOperation = data => (data[toCurrencySelectValue] * amount.value).toFixed(2);
+    const exchangeOperation = (data) => (data[toCurrencySelectValue] * amount.value).toFixed(2);
 
-    if (amount.value == 0){
+    if (amount.value == 0) {
         alert("The amount field can't be empty!");
-    } else {         
-        fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/' + fromCurrencySelectValue + '/' + toCurrencySelectValue + '.json')
-            .then(response => response.json())
-            .then(data => {
+    } else {
+        fetch(
+            "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/" +
+                fromCurrencySelectValue +
+                "/" +
+                toCurrencySelectValue +
+                ".json"
+        )
+            .then((response) => response.json())
+            .then((data) => {
                 let exchangeValue = exchangeOperation(data);
                 currencyExchangeResult.value = exchangeValue;
             });
     }
 };
 
-switchBtn.addEventListener('click', switchCurrencies);
-exchangeBtn.addEventListener('click', exchangeCurrencies);
-
+switchBtn.addEventListener("click", switchCurrencies);
+exchangeBtn.addEventListener("click", exchangeCurrencies);
